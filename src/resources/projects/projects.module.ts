d262bb52f02d3e7330fc9457fe3d006c731bdfb7
AVOM  Brice <maebrie2017@gmail.com>
2024-09-26 11:18:06 +0100

    Update projects.module.ts
    disable invitation service in project module

--- src/resources/projects/projects.module.ts
+++ src/resources/projects/projects.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
 import { Slugify } from 'src/global/utils/slugilfy';
-import { ProjectInvitationService } from './projects.invitation';
+// import { ProjectInvitationService } from './projects.invitation';
 
 @Module({
   controllers: [ProjectsController],
-  providers: [ProjectsService, Slugify, ProjectInvitationService],
+  providers: [
+    ProjectsService, 
+    Slugify,
+    // ProjectInvitationService
+  ],
 })
 export class ProjectsModule { }
